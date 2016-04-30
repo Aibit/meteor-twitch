036bbe756ef6a9e79f769944c2d8cd894d1dc32d
Eduard Montserrat <edu.moga@gmail.com>
2016-04-30 20:35:16 +0200

    Scope working

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "aibit:twitch",
   summary: "Twitch API integration for Meteor",
-  version: "0.1.5",
+  version: "0.1.6",
   git: "https://github.com/Aibit/meteor-twitch.git"
 });
 
